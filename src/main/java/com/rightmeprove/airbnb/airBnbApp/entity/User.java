74bd3d3766c3fbf9e5644b98b9ae6cd94dfac4ab
RightMeProve <officialsatyam2022@gmail.com>
2025-09-29 01:05:17 +0530

    Write JWTService code for Access Token, Refresh Token & getUserIdFromToken

--- src/main/java/com/rightmeprove/airbnb/airBnbApp/entity/User.java
+++ src/main/java/com/rightmeprove/airbnb/airBnbApp/entity/User.java
@@ -4,15 +4,22 @@ import com.rightmeprove.airbnb.airBnbApp.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "app_user")
 // Renamed because "user" is a reserved keyword in Postgres
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,4 +52,16 @@ public class User {
      * → which breaks if enum order changes in code.
      */
     private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_"+role.name()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
